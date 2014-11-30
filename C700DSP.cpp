447109165d287dd339550dbe655e5dbb3d95bd9c
Takahiko Fujita <osoumen@rice-2.local>
2014-11-30 21:40:34 +0900

    dsp用クラスの用意

---
+++ C700DSP.cpp
@@ -0,0 +1,9 @@
+//
+//  C700DSP.cpp
+//  C700
+//
+//  Created by osoumen on 2014/11/30.
+//
+//
+
+#include "C700DSP.h"
---
+++ C700DSP.h
@@ -0,0 +1,14 @@
+//
+//  C700DSP.h
+//  C700
+//
+//  Created by osoumen on 2014/11/30.
+//
+//
+
+#ifndef __C700__C700DSP__
+#define __C700__C700DSP__
+
+#include <iostream>
+
+#endif /* defined(__C700__C700DSP__) */
